328acc482608db12576c4c0bd0899119336a6a21
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-25 04:25:10 +0900

    tweets: GET one tweet

--- api/cruds/tweets.py
+++ api/cruds/tweets.py
@@ -1,15 +1,11 @@
 from typing import List
 
-from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy.orm import selectinload
 
-from api.cruds import auths as auth_api
 from api.models import tweets as tweet_model
 from api.models import users as user_model
 from api.schemas import tweets as tweet_schema
-from api.schemas import users as user_schema
 
 
 async def get_all_tweets(db: AsyncSession) -> List[tweet_model.Tweet]:
@@ -20,6 +16,7 @@ async def create_tweet(
     db: AsyncSession, current_user: user_model.User, tweet_body: tweet_schema.TweetCreate
 ) -> tweet_model.Tweet:
     tweet = tweet_model.Tweet(**tweet_body.dict())
+    # if either one of them is written, this transaction will work properly.
     tweet.user = current_user
     tweet.user_id = current_user.id
 
@@ -28,3 +25,7 @@ async def create_tweet(
     await db.refresh(tweet)
 
     return tweet
+
+
+async def find_by_id(db: AsyncSession, tweet_id: int) -> tweet_model.Tweet | None:
+    return (await db.execute(select(tweet_model.Tweet).filter_by(id=tweet_id))).first()
--- tests/test_routers/test_tweets.py
+++ tests/test_routers/test_tweets.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi import status
 
 from api.schemas import tweets as tweet_schema
+from api.schemas import users as user_schema
 from tests.factories import UserFactory, create_access_token, random_string
 from tests.init_async_client import async_client as client
 
@@ -32,12 +33,49 @@ class TestPostTweet:
 
         resp = await client.post("/tweets", json={"tweet": "my first tweet"}, headers=headers)
         assert resp.status_code == status.HTTP_201_CREATED
+        await client.post("/tweets", json={"tweet": "second tweet"}, headers=headers)
 
         tweet = tweet_schema.TweetCreateResponse(**resp.json())
         assert tweet.tweet == "my first tweet"
 
+        # check user-tweet relation
+        resp = await client.get("/users", headers=headers)
+        user_obj = user_schema.User(**resp.json()[0])
+
+        assert user_obj.id == user.id
+        assert len(user_obj.tweets) == 2
+        assert user_obj.tweets[0].tweet == "my first tweet"
+        assert user_obj.tweets[1].tweet == "second tweet"
+
     async def test_post_tweet_which_has_invalid_request_body(self, client, login_fixture):
         _, headers = await login_fixture
 
-        resp = await client.post("/tweets", json={"spam": "Why I stay woke up?"}, headers=headers)
+        resp = await client.post(
+            "/tweets", json={"spam": "How do I stay wokewoke up?"}, headers=headers
+        )
         assert resp.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.asyncio
+class TestGetTweet:
+    async def test_get_tweet(self, client, login_fixture):
+        _, headers = await login_fixture
+
+        resp = await client.post("/tweets", json={"tweet": "first post"}, headers=headers)
+        assert resp.status_code == status.HTTP_201_CREATED
+
+        tweet_id = resp.json()["id"]
+
+        # get tweet
+        resp = await client.get(f"/tweets/{tweet_id}", headers=headers)
+        assert resp.status_code == status.HTTP_200_OK
+        tweet = tweet_schema.Tweet(**resp.json())
+        assert tweet.tweet == "first post"
+
+    async def test_get_tweet_which_doesnt_exist(self, client, login_fixture):
+        _, headers = await login_fixture
+
+        # get tweet
+        resp = await client.get(f"/tweets/123", headers=headers)
+        assert resp.status_code == status.HTTP_404_NOT_FOUND
+        assert resp.json() == {"detail": "Tweet: 123 Not Found"}
