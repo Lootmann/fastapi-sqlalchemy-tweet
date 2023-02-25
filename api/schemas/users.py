00fc1b86ac9f148a099a83bb396db7da3761ccfc
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-25 09:33:23 +0900

    users: schema validation

--- api/schemas/users.py
+++ api/schemas/users.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from pydantic import BaseModel, Field
+from fastapi import HTTPException, status
+from pydantic import BaseModel, Field, validator
 
 from api.schemas.tweets import Tweet
 
@@ -13,16 +14,45 @@ class UserBase(BaseModel):
 
 
 class UserUpdate(BaseModel):
-    name: str = Field("")
-    password: str = Field("")
+    """
+    name and password are allowed to be empty string.
+    when one of them is empty, this Field can't update.
+    """
+
+    name: str = Field("", max_length=100)
+    password: str = Field("", max_length=100)
+
+    @validator("name")
+    def check_name_length(cls, name: str):
+        if name == "":
+            pass
+        else:
+            if len(name) < 6:
+                raise HTTPException(
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                    detail="Username Length should be longer than 6.",
+                )
+        return name
+
+    @validator("password")
+    def check_password_length(cls, password: str):
+        if password == "":
+            pass
+        else:
+            if len(password) < 6:
+                raise HTTPException(
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                    detail="Password Length should be longer than 6.",
+                )
+        return password
 
     class Config:
         orm_mode = True
 
 
 class UserCreate(BaseModel):
-    name: str
-    password: str
+    name: str = Field(..., min_length=6, max_length=100)
+    password: str = Field(..., min_length=6, max_length=100)
 
     class Config:
         orm_mode = True
