774fd8c89cdd5571af9b8016478d8e782ca5f52d
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-26 09:11:26 +0900

    users: fix cruds import alias

--- api/cruds/users.py
+++ api/cruds/users.py
@@ -6,13 +6,14 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
 
 from api.cruds import auths as auth_api
-from api.models import tweets as tweet_model
-from api.models import users as user_model
+from api.models.likes import Like as LikeModel
+from api.models.tweets import Tweet as TweetModel
+from api.models.users import User as UserModel
 from api.schemas import users as user_schema
 
 
-async def create_user(db: AsyncSession, user_create: user_schema.UserCreate) -> user_model.User:
-    user = user_model.User(**user_create.dict())
+async def create_user(db: AsyncSession, user_create: user_schema.UserCreate) -> UserModel:
+    user = UserModel(**user_create.dict())
     user.password = await auth_api.get_hashed_password(user.password)
 
     db.add(user)
@@ -22,28 +23,31 @@ async def create_user(db: AsyncSession, user_create: user_schema.UserCreate) -> 
     return user
 
 
-async def get_all_users(db: AsyncSession) -> List[user_model.User]:
-    results = await db.execute(select(user_model.User).options(selectinload(user_model.User.tweets)))
+async def get_all_users(db: AsyncSession) -> List[UserModel]:
+    results = await db.execute(select(UserModel).options(selectinload(UserModel.tweets)))
     return results.all()
 
 
-async def find_by_id(db: AsyncSession, user_id: int) -> user_model.User | None:
+async def get_all_users_which_likes_tweet(db: AsyncSession, tweet_id: int) -> List[UserModel]:
+    results = await db.execute(select(UserModel).join(LikeModel).filter(LikeModel.tweet_id == tweet_id)).all()
+    return results
+
+
+async def find_by_id(db: AsyncSession, user_id: int) -> UserModel | None:
     result: Result = await db.execute(
-        select(user_model.User).filter_by(id=user_id).options(selectinload(user_model.User.tweets))
+        select(UserModel).filter_by(id=user_id).options(selectinload(UserModel.tweets))
     )
     user = result.first()
     return user[0] if user else None
 
 
-async def find_by_name(db: AsyncSession, username: str) -> user_model.User:
-    result: Result = await db.execute(select(user_model.User).filter_by(name=username))
+async def find_by_name(db: AsyncSession, username: str) -> UserModel:
+    result: Result = await db.execute(select(UserModel).filter_by(name=username))
     user = result.first()
     return user[0] if user else None
 
 
-async def update_user(
-    db: AsyncSession, updated: user_model.User, user_body: user_schema.UserUpdate
-) -> user_model.User:
+async def update_user(db: AsyncSession, updated: UserModel, user_body: user_schema.UserUpdate) -> UserModel:
     if user_body.name != "":
         updated.name = user_body.name
 
@@ -57,7 +61,7 @@ async def update_user(
     return updated
 
 
-async def delete_user(db: AsyncSession, deleted: user_model.User) -> None:
+async def delete_user(db: AsyncSession, deleted: UserModel) -> None:
     await db.delete(deleted)
     await db.commit()
     return None
