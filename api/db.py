e5b0933d4234d7577b575aa872fd695a9dc00053
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-26 22:00:28 +0900

    db: fix change sessionmaker to async_sessionmaker. but both are also fine in my all test cases.

--- api/db.py
+++ api/db.py
@@ -1,10 +1,12 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 ASYNC_DB_URL = "sqlite+aiosqlite:///dev.db"
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
-async_session = sessionmaker(
+
+# TODO: which do I use sessionmaerk or async_sessionmaker? Both are also fine.
+async_session = async_sessionmaker(
     bind=async_engine,
     class_=AsyncSession,
     autoflush=False,
